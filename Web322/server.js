3bdd68cbca7e054717e473271451a6be452ae63d
Kai <kwilliams114@myseneca.ca>
2024-10-18 23:03:32 -0400

    working files

--- Web322/server.js
+++ Web322/server.js
@@ -6,86 +6,70 @@
 *
 * https://www.senecacollege.ca/about/policies/academic-integrity-policy.html
 *
-* Name: Abdalla Abdelgadir       Student ID: 113734198      Date: 2024-10-15
+* Name: Kai Williams       Student ID: 103714234      Date: 2024-10-18
 *
 * Published URL: ___________________________________________________________
 *
 ********************************************************************************/
 
-const legoData = require('./modules/legoSets') 
+const legoData = require('./modules/legoSets'); 
 const express = require('express'); 
 const app = express();
 const path = require('path');
-const PORT = process.env.PORT || 8080; // Vercel Link: 
+const PORT = process.env.PORT || 8080; // Vercel Link
 
 app.use(express.static(path.join(__dirname, "public")));
 
 
-  app.get("/", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "views", "home.html"));
-  });
+});
 
-  app.get("/about", (req, res) => {
+app.get("/about", (req, res) => {
     res.sendFile(path.join(__dirname, "views", "about.html"));
-  });
+});
 
-  app.get("/404", (req, res) => {
+app.get("/404", (req, res) => {
     res.sendFile(path.join(__dirname, "views", "404.html"));
-  });
-  app.get("/", (req, res) => {
-    res.send("Assignment 3: Abdalla Abdelgadir - 113734198");
-  });
+});
 
+app.get("/lego/sets", (req, res) => {
+    legoData.getAllSets()
+        .then(sets => res.json(sets))
+        .catch(error => res.status(500).send(error));
+});
+
+app.get("/lego/sets/:set_num", (req, res) => {
+    legoData.getSetByNum(req.params.set_num)
+        .then(set => res.json(set))
+        .catch(error => res.status(404).send(error));
+});
+
+app.get("/lego/sets/theme-demo", (req, res) => {
+    legoData.getSetsByTheme("City")
+        .then(sets => res.json(sets))
+        .catch(error => res.status(404).send(error));
+});
 
 
 legoData.initialize()
     .then(() => {
         app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`)
-        })
+            console.log(`Server is running on port ${PORT}`);
+        });
     })
     .catch(error => {
-        console.error("Initialization failed:", error)
-    })
+        console.error("Initialization failed:", error);
+    });
 
-app.get("/", (req, res) => {
-    res.send("Assignment 3: Abdalla Abdelgadir - 113734198")
-})
-
-app.get("/lego/sets", (req, res) => {
-    legoData.getAllSets()
-        .then(sets => res.json(sets))
-        .catch(error => res.status(500).send(error))
-})
-
-app.get("/lego/sets/:set_num", (req, res) => {
-
-    legoData.initialize().then(() => {
-        legoData.getSetByNum(req.params.set_num).then((sets=> {
-            res.send(sets); 
-        }))
-        .catch((err) => {
-            res.send(err); 
-        }); 
-    }); 
-  }); 
-
-app.get("/lego/sets/theme-demo", (req, res) => {
-    legoData.getSetsByTheme("town")
-        .then(sets => res.json(sets))
-        .catch(error => res.status(404).send(error))
-})
-
-/*
-async function run() {
-    await legoData.initialize() // testing functions
-
-    const allSets = legoData.getAllSets()
-    console.log(allSets)
-
-    const specificSet = await legoData.getSetByNum("0003977811-1")
-    console.log(specificSet)
-}
-
-run().catch(console.error)
-*/
+    async function run() {
+        await legoData.initialize() // testing functions
+    
+        const allSets = legoData.getAllSets()
+        console.log(allSets)
+    
+        const specificSet = await legoData.getSetByNum("0003977811-1")
+        console.log(specificSet)
+    }
+    
+    run().catch(console.error)
