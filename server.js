2e9320c998819afdcb385069208db0d79180b4ca
Kai <kwilliams114@myseneca.ca>
2024-11-22 15:05:49 -0500

    fixed views

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ const port = process.env.PORT || 8080
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true })) 
 app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "views")))
 
 app.get("/", (req, res) => {
   res.render("home")
